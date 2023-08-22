0dda7f14149cb7b57d7313530b2c857f2befb513
Angad <angadgpb09876@gmail.com>
2023-08-22 16:26:16 +0530

    after restriction

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-const loginsec =document.querySelector('.login-section')
-const loginlink =document.querySelector('.login-link')
-const registerlink =document.querySelector('.register-link')
-registerlink.addEventListener('click' ,()=>{
+const loginsec = document.querySelector('.login-section')
+const loginlink = document.querySelector('.login-link')
+const registerlink = document.querySelector('.register-link')
+registerlink.addEventListener('click', () => {
     loginsec.classList.add('active')
 })
-loginlink.addEventListener('click' ,()=>{
+loginlink.addEventListener('click', () => {
     loginsec.classList.remove('active')
 })
+
+function myFun() {
+    var name = document.getElementById("User_Name").value;
+    if (name.lengh < 3) {
+        document.getElementById("message").innerHTML = "**username must be 3character"
+        return false;
+    }
+
+    if (name.lengh > 15) {
+        document.getElementById("message").innerHTML = "**username must be 3character"
+        return false;
+    }
+}
+
+function myFun() {
+    let a = document.getElementById("User_Name").value;
+    let currect = /^[A-Za-z]+$/;
+    if (a == "") {
+        document.getElementById("Message").innerHTML = "**username mast be 3character";
+        return false;
+    }
+    if (a.length < 3) {
+        document.getElementById("Message").innerHTML = "**username mast be 3character";
+        return false;
+    }
+    if (a.length > 15) {
+        document.getElementById("Message").innerHTML = "**not allowed after 15character";
+        return false;
+    }
+    if (a.match(currect))
+        true;
+    else {
+        document.getElementById("Message").innerHTML = "**only alphabet are allowed";
+        return false;
+    }
+}
